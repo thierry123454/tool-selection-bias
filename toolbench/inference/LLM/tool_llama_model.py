5d5e6395814101926d14f0c56c516164f5bca348
tb-software-official <tbsoftware12345@gmail.com>
2025-07-11 10:37:05 +0100

    fix small model

--- toolbench/inference/LLM/tool_llama_model.py
+++ toolbench/inference/LLM/tool_llama_model.py
@@ -67,6 +67,7 @@ class ToolLLaMA:
                 "stop_token_ids": None,
                 "echo": False
             }
+            print(f"Prompt fed to model: {prompt}")
             generate_stream_func = generate_stream
             output_stream = generate_stream_func(self.model, self.tokenizer, gen_params, "cuda", self.max_sequence_length, force_generate=True)
             outputs = self.chatio.return_output(output_stream)
